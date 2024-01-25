f9c5b2ee5bdf9b1b9e08d9a66f533459de264446
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-01-26 01:47:23 +0300

    .

--- src/main/java/com/tugalsan/api/sql/insert/server/TS_SQLInsertUtils.java
+++ src/main/java/com/tugalsan/api/sql/insert/server/TS_SQLInsertUtils.java
@@ -1,11 +1,10 @@
 package com.tugalsan.api.sql.insert.server;
 
-import com.tugalsan.api.log.server.*;
 import com.tugalsan.api.sql.conn.server.*;
 
 public class TS_SQLInsertUtils {
 
-    final private static TS_Log d = TS_Log.of(TS_SQLInsertUtils.class);
+//    final private static TS_Log d = TS_Log.of(TS_SQLInsertUtils.class);
 
     public static TS_SQLInsert insert(TS_SQLConnAnchor anchor, CharSequence tableName) {
         return new TS_SQLInsert(anchor, tableName);
